b2035d4448608651b70a09be22ad64da2f390e38
Bhawuk Arora <bhawukarora042@gmail.com>
2024-11-15 16:08:14 +0530

    added initial files

---
+++ src/test/java/com/scm/ScmApp/ScmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.ScmApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
